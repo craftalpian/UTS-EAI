940d6229a37fe491f4af20ddcf7c975d19864263
Alfian <alfiananandaputra@gmail.com>
2024-05-11 00:42:04 +0700

    done

--- restapi/backend/index.js
+++ restapi/backend/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const randomstring = require("randomstring");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/book", (req, res) => {
+  const { id } = req?.body;
+
+  console.timeLog("Received Book Data: ", `[${id}]`);
+
+  res.json({
+    type: "book",
+    id: `book-${id}`,
+  });
+});
+
+app.get("/gadget", (_, res) => {
+  const id = randomstring.generate();
+  console.time("Data")
+  console.timeLog("Data", `[${id}]`);
+  console.timeEnd("Data")
+
+  res.json({
+    type: "gadget",
+    id: `gadget-${id}`,
+  });
 });
 
 app.listen(port, () => {
--- restapi/trigger/index.js
+++ restapi/trigger/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 require("dotenv").config();
+const { default: axios } = require("axios");
 const app = express();
 const port = process.env.PORT;
+const randomstring = require("randomstring");
 
 app.use(express.json());
-app.get("/trigger", (_, res) => {
+app.get("/trigger", async (_, res) => {
   console.time("Loop");
+  const id = randomstring.generate();
 
-  for (let i = 0; i < 1000000; i++) {
-    if (i === 500000) {
-      console.timeLog("Loop", "Reached halfway");
-    }
+  for (let i = 0; i < 10; i++) {
+    const { data: postData } = await axios.post("http://localhost:2000/book", {
+      id,
+    });
+
+    console.timeLog("Loop", { postData });
+
+    const { data: getData } = await axios.get("http://localhost:2000/gadget");
+
+    console.timeLog("Loop", getData?.id);
   }
-
   console.timeEnd("Loop");
 
-  res.send("Hello World!");
+  res.json({
+    status: true,
+  });
 });
 
 app.listen(port, () => {
