9878c9b6d95741fcaf22464a6ada9bca8ca0e124
Alfian <alfiananandaputra@gmail.com>
2024-05-11 08:04:45 +0700

    init: restapi

--- restapi/backend/index.js
+++ restapi/backend/index.js
@@ -1,32 +1,54 @@
-const express = require("express");
+// Menggunakan dotenv untuk mengampil variabel environment
 require("dotenv").config();
-const app = express();
-const port = process.env.PORT;
+// Menampilkan semua log disertai timestamp
+require("log-timestamp");
+// Menggunakan framework ExpressJs
+const express = require("express");
+// Menggunakan randomstring untuk membuat id secara acak
 const randomstring = require("randomstring");
 
+// Melakukan inisiasi aplikasi ExpressJs
+const app = express();
+// Membuat variabel port dari environment
+const port = process.env.PORT;
+
+// Aplikasi Express dapat menerima tipe data JSON
 app.use(express.json());
 
+// Endpoint /book yang akan menerima data dalam bentuk POST
 app.post("/book", (req, res) => {
+  // Mengatur awal waktu proses menerima data sampai mengembalikan data
+  console.time("Received Post");
+
+  // Mengambil id yang dikirimkan dari trigger
   const { id } = req?.body;
 
-  console.timeLog("Received Book Data: ", `[${id}]`);
-
+  // Mengembalikan data berupa JSON dengan id yang dikirimkan oleh trigger
   res.json({
     type: "book",
     id: `book-${id}`,
   });
+
+  // Mengatur proses berhenti mencatat waktu untuk label Received Post
+  console.timeEnd("Received Post", `data diterima dari trigger: ${id}`);
 });
 
+// Endpoint /gadget untuk menerima data yang diambil dalam HTTP GET
 app.get("/gadget", (_, res) => {
-  const id = randomstring.generate();
-  console.time("Data")
-  console.timeLog("Data", `[${id}]`);
-  console.timeEnd("Data")
+// Mengatur awal waktu proses menerima request sampai mengembalikan data
+  console.time("Received Get");
 
+  // Membuat id random dari package randomstring
+  const id = randomstring.generate();
+
+  // Mengembalikan response
   res.json({
     type: "gadget",
     id: `gadget-${id}`,
   });
+
+  // Mengatur proses berhenti mencatat waktu untuk label Received Get
+  console.timeEnd("Received Get", `data yang dikembalikan dengan id: ${id}`);
 });
 
 app.listen(port, () => {
--- restapi/trigger/index.js
+++ restapi/trigger/index.js
@@ -1,28 +1,69 @@
-const express = require("express");
+// Menggunakan dotenv untuk mengampil variabel environment
 require("dotenv").config();
+// Menampilkan semua log disertai timestamp
+require("log-timestamp");
+// Menggunakan framework ExpressJs
+const express = require("express");
+// Menggunakan Axios untuk mengirim data ke backend
 const { default: axios } = require("axios");
-const app = express();
-const port = process.env.PORT;
+// Menggunakan randomstring untuk membuat id secara acak
 const randomstring = require("randomstring");
 
-app.use(express.json());
-app.get("/trigger", async (_, res) => {
-  console.time("Loop");
-  const id = randomstring.generate();
+// Melakukan inisiasi aplikasi ExpressJs
+const app = express();
+// Membuat variabel port dari environment
+const port = process.env.PORT;
 
+// Membuat endpoint terhadap /trigger
+app.get("/trigger", async (_, res) => {
+  // Mengatur awal waktu proses looping data dengan nama Loop
+  console.time("Loop");
+
+  // Melakukan iterasi sebanyak 10 kali ke beberapa service penerima
   for (let i = 0; i < 10; i++) {
+    // Membuat id random yang akan dikirim
+    const id = randomstring.generate();
+
+    /**
+     * Pada percobaan pertama, saya menguji untuk mengirim data ke endpoint /book
+     * dan endpoint tersebut akan menampilkan kembali. Dengan tujuan untuk memahami
+     * seberapa lama waktu yang dibutuhkan untuk mengirimkan data berulang.
+     */
+
+    // Mengatur kapan waktu untuk memulai
+    console.time("LoopPost");
+
+    // Mengirimkan data berupa post dalam JSON dengan variabel id yang dikirim
     const { data: postData } = await axios.post("http://localhost:2000/book", {
       id,
     });
 
-    console.timeLog("Loop", { postData });
+    // Memberhentikan proses perhitungan waktu yang dibutuhkan
+    console.timeEnd(
+      "LoopPost",
+      `id yang dikirim: ${id} | id yang diterima: ${postData?.id}`
+    );
 
+    /**
+     * Pada percobaan kedua, saya menguji untuk mengambil data dari endpoint
+     * /gadget. Hal ini akan mengetahui perbandingan dari melakukan pengiriman data
+     * pada uji pertama dengan pengambilan data pada uji kedua.
+     */
+
+    // Mengatur waktu mulai untuk mendapatkan data
+    console.time("LoopGet");
+
+    // Mengambil data dari endpoint /gadget
     const { data: getData } = await axios.get("http://localhost:2000/gadget");
 
-    console.timeLog("Loop", getData?.id);
+    // Memberhentikan perulangan
+    console.timeEnd("LoopGet", `berhasil mengambil id: ${getData?.id}`);
   }
+
+  // Menyelesaikan waktu dalam proses iterasi sebanyak n-kali mengirimkan data secara bersamaan
   console.timeEnd("Loop");
 
+  // Menampilkan response json
   res.json({
     status: true,
   });
